f603651a7826f3d8c809fb033480b28077c472ff
haxxxs <99824472+haxxxs@users.noreply.github.com>
2024-03-27 01:57:01 +0400

    starting vue app

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import MyHello from './components/MyHello.vue'
+</script>
+
+<template>
+  <MyHello title="Кроссовки Puma" :price="19500" :isAdded="true" />
+  <MyHello />
+  <MyHello />
+</template>
